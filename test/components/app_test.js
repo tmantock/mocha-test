6e2c39eeda3703f307434dc2344996e3f9a972d6
Tevin Mantock <tmantock@gmail.com>
2016-08-13 18:16:03 -0700

    initial commit and first test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,18 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+//Use 'describe' to group together similar tests
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  //Use 'it' to test a single attricute of a target
+  it('shows the correct text', () => {
+
+    //creat am instance of App
+    const component = renderComponent(App);
+
+    //use 'expect' to make an assertion about a target
+    expect(component).to.contain('React simple starter');
+
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
 });
