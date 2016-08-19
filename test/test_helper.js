60344302344e9e290b09cfed6f0608b555d28ce3
Tevin Mantock <tmantock@gmail.com>
2016-08-18 20:28:12 -0700

    importing jquery

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,10 @@
 import jsdom from 'jsdom';
+import jquery from 'jquery';
 //Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+const $ = jquery(global.window);
+
 // Build 'renderComponent' helper that should render a given react ComponentClass
 
 //Build helper for simulating events
