3e114731d6184a636f6d5ca2953be84bd44c53aa
Tevin Mantock <tmantock@gmail.com>
2016-08-18 20:23:05 -0700

    initializing jsdom

---
+++ test/test_helper.js
@@ -0,0 +1,9 @@
+import jsdom from 'jsdom';
+//Set up testing environment to run like a browser in the command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+// Build 'renderComponent' helper that should render a given react ComponentClass
+
+//Build helper for simulating events
+
+//Set-up Chai-jQuery
